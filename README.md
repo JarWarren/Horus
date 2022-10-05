c75ac3fcd38f3315193b9fa0033f90963df510a6
JarWarren <jarwarren@meta.com>
2022-10-04 18:47:49 -0600

    vertex and fragment in separate files

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Horus
 
 WGSL shader renderer
+
+## TODO
+
+- use uniforms successfully
+    - make it a consistent experience with the book of shaders and shadertoy
+- pass in and use a custom fragment shader (from command line)
+- simplify imports
+- trim code to the bare minimum
+- annotate thoroughly
+- export as a binary and verify that it works
+- better command line parsing (find a crate)
+    - offer help
+    - create new, default shader
+    - run a shader
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use std::env::args;
 use std::fs::read_to_string;
+use std::time::Instant;
 
+use wgpu::{
+    Backends, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, BlendState, BufferBindingType,
+    BufferUsages, Color, ColorTargetState, ColorWrites, CommandEncoderDescriptor, Device, DeviceDescriptor, Face, Features,
+    FragmentState, FrontFace, Instance, Limits, LoadOp, MultisampleState, Operations, PipelineLayoutDescriptor, PolygonMode, PowerPreference, PresentMode,
+    PrimitiveState, PrimitiveTopology, RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor, RequestAdapterOptions, ShaderModuleDescriptor,
+    ShaderSource, ShaderStages, Surface, SurfaceConfiguration, TextureUsages, TextureViewDescriptor, util::{BufferInitDescriptor, DeviceExt}, VertexState,
+};
 use winit::{
     event::*,
-    event_loop::{ControlFlow, EventLoop},
+    event_loop,
     window::WindowBuilder,
 };
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniforms {
+    mouse: [f32; 2],
+    resolution: [f32; 2],
+    time: f32,
+}
 
 fn main() {
     pollster::block_on(run());
@@ -15,7 +31,7 @@ async fn run() {
     env_logger::init();
 
     // context for retrieving events from the system
-    let event_loop = EventLoop::new();
+    let event_loop = event_loop::EventLoop::new();
 
     // register a new window within the context
     let window = WindowBuilder::new()
@@ -25,15 +41,15 @@ async fn run() {
     let size = window.inner_size();
 
     // wgpu
-    let instance = wgpu::Instance::new(wgpu::Backends::all());
+    let instance = Instance::new(Backends::all());
 
     // winit window -> wgpu window
     let mut surface = unsafe { instance.create_surface(&window) };
 
     // graphics card
     let adapter = instance.request_adapter(
-        &wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
+        &RequestAdapterOptions {
+            power_preference: PowerPreference::default(),
             compatible_surface: Some(&surface),
             force_fallback_adapter: false,
         },
@@ -42,28 +58,28 @@ async fn run() {
     // device - logical representation of the graphics card
     // queue - how we assign work to the device
     let (device, queue) = adapter.request_device(
-        &wgpu::DeviceDescriptor {
-            features: wgpu::Features::empty(),
-            limits: wgpu::Limits::default(),
+        &DeviceDescriptor {
+            features: Features::empty(),
+            limits: Limits::default(),
             label: None,
         },
         None, // Trace path
     ).await.unwrap();
 
     // configure the surface
-    let mut config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+    let mut config = SurfaceConfiguration {
+        usage: TextureUsages::RENDER_ATTACHMENT,
         format: surface.get_supported_formats(&adapter)[0],
         width: size.width,
         height: size.height,
-        present_mode: wgpu::PresentMode::Fifo, // basically vsync
+        present_mode: PresentMode::Fifo, // basically vsync
     };
     surface.configure(&device, &config);
 
     // vertex shader
-    let vertex_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+    let vertex_shader = device.create_shader_module(ShaderModuleDescriptor {
         label: Some("Vertex Shader"),
-        source: wgpu::ShaderSource::Wgsl(include_str!("vertex.wgsl").into()),
+        source: ShaderSource::Wgsl(include_str!("vertex.wgsl").into()),
     });
 
     // fragment shader
@@ -72,58 +88,78 @@ async fn run() {
         fragment_path = args().nth(1).unwrap();
     }
     let fragment_source = read_to_string(&fragment_path).unwrap();
-    let fragment_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+    let fragment_shader = device.create_shader_module(ShaderModuleDescriptor {
         label: Some("Fragment Shader"),
-        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(&fragment_source)),
+        source: ShaderSource::Wgsl(std::borrow::Cow::Borrowed(&fragment_source)),
+    });
+
+    // uniform data to be sent to the shaders
+    let mut uniforms = Uniforms { mouse: [0., 0.], resolution: [size.width as _, size.height as _], time: 0. };
+    let time = Instant::now();
+    let uniforms_buffer = device.create_buffer_init(&BufferInitDescriptor {
+        label: None,
+        contents: bytemuck::bytes_of(&uniforms),
+        usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+    });
+
+    let uniforms_buffer_layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+        label: None,
+        entries: &[BindGroupLayoutEntry {
+            binding: 0,
+            visibility: ShaderStages::FRAGMENT,
+            count: None,
+            ty: BindingType::Buffer {
+                ty: BufferBindingType::Uniform,
+                has_dynamic_offset: false,
+                min_binding_size: None,
+            },
+        }],
+    });
+
+    let uniforms_buffer_bind_group = device.create_bind_group(&BindGroupDescriptor {
+        label: None,
+        layout: &uniforms_buffer_layout,
+        entries: &[BindGroupEntry {
+            binding: 0,
+            resource: uniforms_buffer.as_entire_binding(),
+        }],
     });
 
     // determines which resources are bound to the pipeline
     let render_pipeline_layout =
-        device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        device.create_pipeline_layout(&PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[],
+            bind_group_layouts: &[&uniforms_buffer_layout],
             push_constant_ranges: &[],
         });
 
     // represents all stages of the rendering process
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+    let render_pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
         label: Some("Render Pipeline"),
         layout: Some(&render_pipeline_layout),
-        vertex: wgpu::VertexState {
+        vertex: VertexState {
             module: &vertex_shader,
             entry_point: "vs_main",
             buffers: &[],
         },
-        fragment: Some(wgpu::FragmentState {
+        fragment: Some(FragmentState {
             module: &fragment_shader,
             entry_point: "fs_main",
-            targets: &[Some(wgpu::ColorTargetState {
+            targets: &[Some(ColorTargetState {
                 format: config.format,
-                blend: Some(wgpu::BlendState::REPLACE),
-                write_mask: wgpu::ColorWrites::ALL,
+                blend: Some(BlendState::REPLACE),
+                write_mask: ColorWrites::ALL,
             })],
         }),
-        primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList,
-            strip_index_format: None,
-            front_face: wgpu::FrontFace::Ccw,
-            cull_mode: Some(wgpu::Face::Back),
-            polygon_mode: wgpu::PolygonMode::Fill,
-            unclipped_depth: false,
-            conservative: false,
-        },
+        primitive: PrimitiveState::default(),
         depth_stencil: None,
-        multisample: wgpu::MultisampleState {
-            count: 1,
-            mask: !0,
-            alpha_to_coverage_enabled: false,
-        },
+        multisample: MultisampleState::default(),
         multiview: None,
     });
 
     // continuously poll window events from the system
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
+        *control_flow = event_loop::ControlFlow::Poll;
         match event {
             Event::MainEventsCleared => window.request_redraw(),
             Event::WindowEvent {
@@ -140,74 +176,102 @@ async fn run() {
                             ..
                         },
                         ..
-                    } => *control_flow = ControlFlow::Exit,
+                    } => *control_flow = event_loop::ControlFlow::Exit,
                     WindowEvent::Resized(physical_size) => {
-                        resize(&device, &mut surface, &mut config, *physical_size);
+                        resize(&device, &mut surface, &mut config, *physical_size, &mut uniforms);
                     }
                     WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        resize(&device, &mut surface, &mut config, **new_inner_size);
+                        resize(&device, &mut surface, &mut config, **new_inner_size, &mut uniforms);
+                    }
+                    WindowEvent::CursorMoved { position, .. } => {
+                        uniforms.mouse = [position.x as _, position.y as _];
                     }
                     _ => {}
                 }
             }
             Event::RedrawRequested(window_id) if window_id == window.id() => {
-                match render(&device, &surface, &queue, &render_pipeline) {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => resize(&device, &mut surface, &mut config, size),
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // We're ignoring timeouts
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+                // get a SurfaceTexture to render to
+                let output = surface.get_current_texture().unwrap();
+                let view = output.texture.create_view(&TextureViewDescriptor::default());
+
+                // update uniforms
+                uniforms.time = time.elapsed().as_secs_f32();
+                queue.write_buffer(&uniforms_buffer, 0, bytemuck::bytes_of(&uniforms));
+
+                // the encoder will create a command buffer to send to the device
+                let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor {
+                    label: Some("Render Encoder"),
+                });
+
+                {
+                    let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &[Some(RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: Operations {
+                                load: LoadOp::Clear(Color::BLACK),
+                                store: true,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                    });
+                    render_pass.set_pipeline(&render_pipeline);
+                    render_pass.set_bind_group(0, &uniforms_buffer_bind_group, &[]);
+                    render_pass.draw(0..3, 0..1);
                 }
+
+                queue.submit(std::iter::once(encoder.finish()));
+                output.present();
             }
             _ => {}
         }
     });
 }
 
-fn resize(device: &wgpu::Device, surface: &mut wgpu::Surface, config: &mut wgpu::SurfaceConfiguration, new_size: winit::dpi::PhysicalSize<u32>) {
+fn resize(device: &Device, surface: &mut Surface, config: &mut SurfaceConfiguration, new_size: winit::dpi::PhysicalSize<u32>, uniforms: &mut Uniforms) {
     if new_size.width > 0 && new_size.height > 0 {
         config.width = new_size.width;
         config.height = new_size.height;
+        uniforms.resolution = [new_size.width as _, new_size.height as _];
         surface.configure(device, config);
     }
 }
 
-fn render(device: &wgpu::Device, surface: &wgpu::Surface, queue: &wgpu::Queue, pipeline: &wgpu::RenderPipeline) -> Result<(), wgpu::SurfaceError> {
-    // get a SurfaceTexture to render to
-    let output = surface.get_current_texture()?;
-    let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-    // the encoder will create a command buffer to send to the device
-    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-        label: Some("Render Encoder"),
-    });
-
-    {
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("Render Pass"),
-            color_attachments: &[
-                // This is what @location(0) in the fragment shader targets
-                Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-                        store: true,
-                    },
-                })
-            ],
-            depth_stencil_attachment: None,
-        });
-
-        render_pass.set_pipeline(&pipeline);
-        render_pass.draw(0..3, 0..1);
-    }
-
-    // submit will accept anything that implements IntoIter
-    queue.submit(std::iter::once(encoder.finish()));
-    output.present();
-
-    Ok(())
-}
+// fn render(device: &Device, surface: &Surface, queue: &Queue, pipeline: &RenderPipeline) -> Result<(), SurfaceError> {
+//     // get a SurfaceTexture to render to
+//     let output = surface.get_current_texture()?;
+//     let view = output.texture.create_view(&TextureViewDescriptor::default());
+//
+//     // the encoder will create a command buffer to send to the device
+//     let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor {
+//         label: Some("Render Encoder"),
+//     });
+//
+//     {
+//         let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+//             label: Some("Render Pass"),
+//             color_attachments: &[
+//                 // This is what @location(0) in the fragment shader targets
+//                 Some(RenderPassColorAttachment {
+//                     view: &view,
+//                     resolve_target: None,
+//                     ops: Operations {
+//                         load: LoadOp::Clear(Color::BLACK),
+//                         store: true,
+//                     },
+//                 })
+//             ],
+//             depth_stencil_attachment: None,
+//         });
+//
+//         render_pass.set_pipeline(&pipeline);
+//         render_pass.draw(0..3, 0..1);
+//     }
+//
+//     // submit will accept anything that implements IntoIter
+//     queue.submit(std::iter::once(encoder.finish()));
+//     output.present();
+//
+//     Ok(())
+// }
